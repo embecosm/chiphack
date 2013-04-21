49f23f28d031b18da875396133a671dfb154c0f0
Jeremy Bennett <jeremy.bennett@embecosm.com>
2013-04-21 10:39:01 +0100

    Add support for generating VCDs.
      * basics_main.cpp (main): Add VCD generating code.

--- src/verilog_sessions/basics/basics_main.cpp
+++ src/verilog_sessions/basics/basics_main.cpp
@@ -22,6 +22,10 @@
 
 #include "Vbasics.h"
 #include "verilated.h"
+#if VM_TRACE
+# include <verilated_vcd_c.h>	// Trace file format header
+#endif
+
 #include <iostream>
 
 
@@ -31,6 +35,7 @@ int main (int   argc,
 {
   Verilated::commandArgs (argc, argv);
   Vbasics *top = new Vbasics;
+
   int  clk;
   int  led;
   int  old_led;
@@ -38,18 +43,31 @@ int main (int   argc,
   int  reset;
   int  button;
 
+  vluint64_t main_time = 0;	// Current simulation time (64-bit unsigned)
+
   clk = 0;
   reset = 1;
   button = 1;
   key = (button << 1) | reset;
   old_led = led - 1;
 
-  while (1)
+#if VM_TRACE
+  // Set if called with --trace
+  Verilated::traceEverOn(true);
+  VerilatedVcdC *tracer = new VerilatedVcdC;
+  top->trace (tracer, 99);
+  tracer->open ("basics_dump.vcd");
+#endif
+
+  do
     {
       top->KEY = key;
       top->CLOCK_50 = clk;
 
       top->eval ();
+#if VM_TRACE
+      tracer->dump (main_time);
+#endif
 
       led = top->LED;
       if (led != old_led)
@@ -59,5 +77,14 @@ int main (int   argc,
 	}
 
       clk = 1 - clk;			// Advance the clock
+      main_time += 1;			// One "time unit" per clock edge.
     }
+  while (led != 0xfc);
+
+  top->final();
+
+#if VM_TRACE
+  tracer->close ();
+#endif
+
 }
